6a697ec50a0fe7b2c59faeb0000b5cbaa79b1f5d
Rafael Jarosch <info.uni.rafa@gmail.com>
2018-12-04 16:52:48 +0100

    remove pdf's

--- Project/ADMM_Optimization/Release/lib/Core/Memory.d
+++
@@ -1,9 +0,0 @@
-lib/Core/Memory.d: ../lib/Core/Memory.cpp ../lib/Core/Memory.hpp \
- /home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.hpp \
- /home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.h
-
-../lib/Core/Memory.hpp:
-
-/home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.hpp:
-
-/home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.h:
--- Project/ADMM_Optimization/Release/lib/Core/StrError.d
+++
@@ -1,6 +0,0 @@
-lib/Core/StrError.d: ../lib/Core/StrError.c ../lib/Core/StrError.h \
- /home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.h
-
-../lib/Core/StrError.h:
-
-/home/sunkg/workspace/OpenCLExercise1_Basics/lib/Core/Util.h:
