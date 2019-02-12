05de962d040b83b3a1023adafa90ed2d5bf16405
Marvin Knodel <st149003@stud.uni-stuttgart.de>
2019-02-12 20:55:06 +0100

    vergleicher implementiert

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -27,6 +27,7 @@
 #include "GpuImplementation.h"
 #include "CPUImplementation.h"
 #include <Core/Time.hpp>
+#include <Core/Image.hpp>
 
 /**
         Main-Method
@@ -49,6 +50,35 @@ int main(int argc, char** argv) {
 
     Core::TimeSpan cpuExecute = cpuEnd - cpuStart;
     cpuI.printTimeMeasurement(cpuExecute);
+
+	//check if the result image is the same
+	std::size_t inputWidth, inputHeight;
+	std::vector<float> inputDataCPU, inputDataGPU;
+	Core::readImagePGM("output_CannyEdge_CPU.pgm", inputDataCPU, inputWidth, inputHeight);
+	Core::readImagePGM("output_CannyEdge.pgm", inputDataGPU, inputWidth, inputHeight);
+
+	std::size_t errorCount = 0;
+	for (size_t i = 0; i < inputWidth; i = i + 1) { //loop in the x-direction
+		for (size_t j = 0; j < inputHeight; j = j + 1) { //loop in the y-direction
+			size_t index = i + j * inputWidth;
+			// Allow small differences between CPU and GPU results (due to different rounding behavior)
+			if (!(std::abs(inputDataCPU[index] - inputDataGPU[index]) <= 1e-5)) {
+				if (errorCount < 10)
+					std::cout << "Result for " << i << "," << j << " is incorrect: GPU value is " << inputDataGPU[index] << ", CPU value is " << inputDataCPU[index] << std::endl;
+				else if (errorCount == 10)
+					std::cout << "..." << std::endl;
+				errorCount++;
+			}
+		}
+	}
+	if (errorCount != 0) {
+		std::cout << "Found " << errorCount << " differences" << std::endl;
+		return 1;
+	}
+
+	std::cout << std::endl;
+
+	std::cout << "Keine relevanten Unterschiede entdeckt" << std::endl;
 
     return 0;
 }
